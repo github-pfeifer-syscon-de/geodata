b51de769f96f17c01fa16b09a32f1c86eb154d15
Roland Pfeifer <github@pfeifer-syscon.de>
2023-05-18 16:09:35 +0200

    iwin comapat

--- src/MapProjection.cpp
+++ src/MapProjection.cpp
@@ -15,6 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <glibmm.h>
 #include <GenericGlmCompat.hpp> // providers pi constants for windows
 #include <cmath>
 
